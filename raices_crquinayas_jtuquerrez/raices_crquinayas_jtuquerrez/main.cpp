60ece7a51b98a7f6d1a2c8a61e59e98aaebb8803
Sebastian Tuquerrez <jtuquerrez@unicauca.edu.co>
2023-09-13 11:59:02 -0500

    Implementacion de metodos biseccion, regla falsa, newton_raphson y el namespace solucion

---
+++ raices_crquinayas_jtuquerrez/raices_crquinayas_jtuquerrez/main.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include <string>
+#include <cstdlib>
+
+#include "biseccion.h"
+#include "newton_raphson.h"
+#include "reglafalsa.h"
+#include "Expression.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::system;
+using raices::newton_raphson;
+using raices::biseccion;
+using raices::reglafalsa;
+
+/**
+* @brief Solicitud de datos para calcular de la funcion ingresada dentro del intervalo dado utilizando el metodo de biseccion
+* @param str_f Texto de la funcion a evaluar
+*/
+void DatosBiseccion(string str_f);
+
+/**
+* @brief Solicitud de datos para calcular de la funcion ingresada dentro del intervalo dado utilizando el metodo de regla falsa
+* @param str_f Texto de la funcion a evaluar
+*/
+void DatosReglaFalsa(string str_f);
+
+int main (int argc, char *argv[]) {
+	int opcion;
+	char aux;
+	
+	do {
+		system("cls");
+		cout << "\n";
+		cout << "================MENU================" << endl;
+		cout << "1. Calculo de una raiz de f(x) = x^2 - cos(x) utilizando el metodo de biseccion." << endl;
+		cout << "2. Calculo de una raiz de f(x) = x^2 - cos(x) utilizando el metodo de regla falsa." << endl;
+		cout << "3. Calculo de una raiz de f(x) = x^3 + 4x^2 - 10, utilizando el metodo de biseccion." << endl;
+		cout << "4. Calculo de una raiz de f(x) = x^3 + 4x^2 - 10, utilizando el metodo de regla falsa." << endl;
+		cout << "5. Salir." << endl;
+		cout << "Ingrese su eleccion (1-5): ";
+		
+		if (!(cin >> opcion)) {
+			cout << "Entrada no valida. Por favor, ingrese un numero valido." << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		
+		cout << "\n";
+		switch (opcion) {
+		case 1:
+			DatosBiseccion("x^2 - cos(x)");
+			break;
+		case 2:
+			DatosReglaFalsa("x^2 - cos(x)");
+			break;
+		case 3:
+			DatosBiseccion("x^3 + 4*x^2 - 10");
+			break;
+		case 4:
+			DatosReglaFalsa("x^3 + 4*x^2 - 10");
+			break;
+		case 5:
+			cout << "Saliendo del programa..." << endl;
+			break;
+		default:
+			cout << "Opcion no valida. Por favor, ingrese una opcion valida (1-5)." << endl;
+		}
+		
+		cout << "\n";
+		cout << "Presione una tecla para continuar....." << endl;
+		cin.get(aux);
+		cin.get(aux);
+	} while (opcion != 5 ); 
+	
+	return 0;
+}
+
+void DatosBiseccion(string str_f) {
+	double xa, xb, tol;
+	int n;
+	
+	cout << "Metodo de biseccion" << endl;
+	cout << "Funcion a evaluar: " << str_f << endl;
+	cout << "Ingrese el valor inferior del intervalo: ";
+	cin >> xa;
+	cout << "Ingrese el valor superior del intervalo: ";
+	cin >> xb;
+	cout << "Ingrese la tolerancia (en porcentaje): ";
+	cin >> tol;
+	cout << "Ingrese el maximo numero de iteraciones: ";
+	cin >> n;
+	
+	//Crear una instancia de biseccion pasando 
+	//la funcion como parametro
+	biseccion bis(str_f);
+	
+	solucion sol = bis.calcular(xa, xb, tol, n);
+	
+	sol.imprimir();
+}
+
+void DatosReglaFalsa(string str_f) {
+	double xi, xs, tol;
+	int n;
+	
+	cout << "Metodo de regla falsa" << endl;
+	cout << "Funcion a evaluar: " << str_f << endl;
+	cout << "Ingrese el valor inferior del intervalo: ";
+	cin >> xi;
+	cout << "Ingrese el valor superior del intervalo: ";
+	cin >> xs;
+	cout << "Ingrese la tolerancia (en porcentaje): ";
+	cin >> tol;
+	cout << "Ingrese el maximo numero de iteraciones: ";
+	cin >> n;
+	//Crear una instancia de regla falsa pasando 
+	//la funcion como parametro
+	
+	reglafalsa rf(str_f);
+	
+	solucion sol = rf.calcular(xi, xs, tol, n);
+	
+	sol.imprimir();
+}
+
+
---
+++ raices_crquinayas_jtuquerrez/raices_crquinayas_jtuquerrez/solucion.h
@@ -0,0 +1,78 @@
+#ifndef SOLUCION_H
+#define SOLUCION_H
+
+#include <cfloat>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::vector;
+
+namespace raices {
+	
+	/**
+	* @brief Calcula el Error Relativo Porcentual entre dos aproximaciones
+	* @param nueva Nueva aproximacion
+	* @param anterior Aproximacion anterior
+	*/
+	double calcular_erp(double nueva, double anterior) {
+		return fabs((nueva - anterior)/nueva) * 100.0f;
+	}
+	
+	/**
+	* @brief Aproximacion de una solucion
+	*/
+	struct aproximacion {
+		double xAnt;/*!< Valor anterior */
+		double xNueva;/*!< Valor anterior */
+		double erp;/*!< Error relativo porcentual entre los dos valores */
+	};
+	
+	/**
+	* @brief Solucion a un metodo numerico para encontrar una raiz
+	*/
+	struct solucion {
+		double raiz = NAN; /*!< Raiz encontrada, NAN si no se pudo hallar */
+		vector<aproximacion> aproximaciones; /*!< Vector de aproximaciones */
+		int iteraciones;
+		
+		/**
+		* @brief Adiciona una nueva aproximacion
+		* @param a Aproximacion
+		*/
+		void adicionar(aproximacion a) {
+			aproximaciones.push_back(a);
+		}
+		
+		void imprimir() {
+			if (std::isnan(raiz)) {
+				cout << "No se pudo obtener la raiz con los datos dados"<<endl;
+			}else {
+				cout << "La raiz es: " << raiz <<endl;
+				cout << "Aproximaciones: "<< endl;
+				cout << setw(20) << "1er aproximacion" << setw(20) << "2da aproximacion" << setw(20) << "Epr" << setw(20) << "No. iteracion" << endl;
+				for (unsigned int i = 0; i< aproximaciones.size(); i++) {
+					cout << setw(20)
+						<< setprecision(10)
+						<< aproximaciones[i].xAnt
+						<< setw(20)
+						<< setprecision(10)
+						<< aproximaciones[i].xNueva
+						<< setw(20)
+						<<setprecision(10)
+						<< aproximaciones[i].erp
+						<< setw(20)
+						<< i+1
+						<<endl;
+				}
+			}
+		}
+	};
+};
+
+#endif

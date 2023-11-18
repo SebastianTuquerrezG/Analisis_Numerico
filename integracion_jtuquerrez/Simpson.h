9e874f4984425a0f9005ee87789e9054b009d580
Sebastian TG <jtuquerrez@unicauca.edu.co>
2023-11-18 18:47:25 -0500

    Integracion mediante tablas de datos

---
+++ integracion_jtuquerrez/Simpson.h
@@ -0,0 +1,19 @@
+#ifndef SIMPSON_H
+#define SIMPSON_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Expression.h"
+#include "util.h"
+
+using std::string;
+using std::vector;
+
+using util::imprimir_tabla;
+using util::crear_tabla;
+
+
+
+#endif
